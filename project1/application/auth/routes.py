a41efa6fe0c70c3b1d520de58ceb201fc0f94303
Melvin <melvinkokxw@gmail.com>
2020-05-07 00:55:53 +0800

    Add hashing for password

--- project1/application/auth/routes.py
+++ project1/application/auth/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, session, flash
+from werkzeug import generate_password_hash, check_password_hash
 
 from application import db
 from .forms import RegistrationForm, LoginForm
@@ -17,7 +18,7 @@ def register():
     if form.validate_on_submit():
         # Get data from form
         username = form.username.data
-        password = form.password.data
+        password = generate_password_hash(form.password.data, method="SHA256")
 
         # If username already exists, reject
         if db.execute("SELECT * FROM users WHERE username=:username", {"username": username}).first() is not None:
@@ -31,7 +32,7 @@ def register():
 
         session["username"] = username
         flash("Registration successful", "success")
-        return render_template("index.html")
+        return redirect(url_for("main.index"))
     return render_template("auth/register.html", form=form)
 
 
@@ -56,7 +57,7 @@ def login():
             return redirect(url_for("auth.login"))
 
         # If wrong password, reject
-        if password != user["password"]:
+        if not check_password_hash(user["password"], password):
             flash("Wrong password", "warning")
             return redirect(url_for("auth.login"))
 
